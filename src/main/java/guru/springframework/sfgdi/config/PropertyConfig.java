97ac154f0562225583bd87ec180f6e0d4af90db7
Christian Preß <Christian.Press@syracos.com>
2020-03-21 16:33:24 +0100

    added property processing of system variable

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
@@ -19,10 +21,13 @@ public class PropertyConfig {
     @Value("${guru.dburl}")
     String url;
 
+    @Autowired
+    Environment environment;
+
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(environment.getProperty("USERNAME")); //os username
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
 
